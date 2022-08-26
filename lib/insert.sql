fe1254aaf3ee2d830a406cab094af7f2de1d6567
MarkBrianKariuki <markbrian.karanja@student.moringaschoool.com>
2022-08-26 15:55:26 +0300

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 19, "M", "light", "melancholic", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 42, "M", "light", "melancholic", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 21, "F", "light", "melancholic", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 13, "M", "light", "melancholic", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 16, "F", "light", "melancholic", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 80, "M", "light", "melancholic", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 35, "F", "light", "melancholic", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 54, "M", "light", "melancholic", FALSE);
